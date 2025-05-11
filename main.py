43410be9175f889f80dda6a98c83a277562c5487
ujju <ujjawalpratap24@gmail.com>
2025-05-11 21:27:10 +0530

    fastapi added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from uv-fastapi-docker!")
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def main():
+    return "hello from new project"
 
 if __name__ == "__main__":
     main()
